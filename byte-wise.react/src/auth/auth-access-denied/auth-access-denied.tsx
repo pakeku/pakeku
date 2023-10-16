eebfafca63fddc2f74b7f875f9fc29c731c29781
pakeku <elina13@wgu.edu>
2023-10-15 20:16:49 -0400

    Clearn up project

--- byte-wise.react/src/auth/auth-access-denied/auth-access-denied.tsx
+++ byte-wise.react/src/auth/auth-access-denied/auth-access-denied.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'; // Assuming you are using React Router
 
 export const AuthAccessDenied: React.FC = () => {
   return (
-    <div className='container d-flex flex-column justify-content-center align-items-center vh-100'>
+    <div className="container d-flex flex-column justify-content-center align-items-center vh-100">
       <div>
         <h1>Access Denied</h1>
         <p>You do not have the required permissions to access this page.</p>
