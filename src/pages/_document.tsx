36e8b3c5f0be56617f1c89ca461b773d7a07bc41
Erick Pacheco <elina13@wgu.edu>
2023-11-03 20:53:17 -0500

    Update _document.tsx
    fix html > jsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,34 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-const MyDocument = () => (
-  <Html>
-    <Head>
-      <!-- Google Tag for Search -->
-        <meta name="google-site-verification" content="cGuD4YnwyVHkoRBCKhF4is0iYsWubpzH6t9mhjsdx6U" />
-        
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-        crossOrigin="anonymous"
-      />
-    </Head>
-    <body>
-      <Main />
-      <script
-        async
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossOrigin="anonymous"
-      />
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Google Tag for Search */}
+          <meta name="google-site-verification" content="cGuD4YnwyVHkoRBCKhF4is0iYsWubpzH6t9mhjsdx6U" />
+          
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <script
+            async
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossOrigin="anonymous"
+          />
 
-      <NextScript />
-    </body>
-  </Html>
-)
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-export default MyDocument
+export default MyDocument;
